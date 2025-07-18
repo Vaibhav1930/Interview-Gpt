ed392a9973433a43c9f40b33dcb4959f34010cab
Vaibhav1930 <159485923+Vaibhav1930@users.noreply.github.com>
2025-07-18 16:12:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ recognition.interimResults = true;
 
 // Instantiate model safely
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash-preview-04-17' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-Pro' });
 
 function App() {
   const [islistening, setIsListening] = useState(false);
